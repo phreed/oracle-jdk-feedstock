65f5c54404e803899f1c4555a427fead91689f8c
fred eisele <fred.eisele@vanderbilt.edu>
2022-05-13 11:25:15 -0500

    bash: problem with deactivate.sh

--- recipe/deactivate.bat
+++ recipe/deactivate.bat
@@ -1,7 +1,8 @@
 rem header is written by bld.bat
 
-rem  set "PKG_UUID=%PKG_NAME%-%PKG_VERSION%_%PKG_BUILDNUM%"
-set "DEACTIVATE_SCRIPT=%CONDA_PREFIX%\conda-activate-meta\%PKG_UUID%\deactivate.bat"
+set "PKG_UUID=%PKG_NAME%-%PKG_VERSION%_%PKG_BUILDNUM%"
+set "CONDA_MESO=%CONDA_PREFIX%\conda-meso\${PKG_UUID}"
+set "DEACTIVATE_SCRIPT=%CONDA_MESO%\deactivate.bat"
 
 if exist "%DEACTIVATE_SCRIPT%" do (
   call "%DEACTIVATE_SCRIPT%"
